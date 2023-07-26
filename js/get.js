27381681f171a3aa9f1b738f63d6d5e7782d5dad
Isaias Leonel Sardina <isaias.sardina@gmail.com>
2023-07-26 02:31:34 -0300

    Agrego JSON para iterar por cada producto

---
+++ js/get.js
@@ -0,0 +1,25 @@
+"use strict";
+import { bodega } from './bd.js';
+
+function mostrar() {
+    let card = document.querySelector('#card-get');
+    card.innerHTML = '';
+    for (const vino of bodega) {
+        card.innerHTML +=
+            ` <div class="card-container">
+            <div>
+            <img src= "${vino.thumb}" class="card-container__img" width= "120" height = "20"/>
+            <div class="card-body">
+                <p class="card-container__trip-name" >${vino.name}</p>
+                <div class="card-container__date">
+                    <p class="oferta"> ¡Oferta relampago!</p>
+                </div>
+                <p class="card-container__price">$${vino.price}</p>
+                <button class="card-container__btn-pay">AGREGAR</button>
+            </div>
+        </div>
+    </div>`;
+    }
+}
+mostrar();
+
--- js/js.js
+++ js/js.js
@@ -3,26 +3,36 @@
 let enviar = document.querySelector("#enviar");
 enviar.addEventListener("click", verificarFormulario);
 
+let regEx = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+
 function verificarFormulario(e) {
   e.preventDefault();
-  let datosRegis = document.querySelector("#datosRegis");
-  let nombre = document.querySelector("#name");
+  let dateRegister = document.querySelector("#datosRegis");
+  let name = document.querySelector("#name");
   let email = document.querySelector("#email");
-  if (nombre.value === "" || email.value === "") {
-    datosRegis.innerHTML = "Por favor rellene todas las casillas";
-    datosRegis.classList.add("danger");
-    datosRegis.classList.remove("correct");
+  let emailOk = regEx.test(email.value);
+  console.log(emailOk);
+
+  if (email.value === "" || name.value === "" || !emailOk) {
+    dateRegister.innerHTML = "Por favor rellene todas las casillas e ingrese un email valido";
+    dateRegister.classList.add("danger");
+    dateRegister.classList.remove("correct");
     setTimeout(() => {
-      datosRegis.innerHTML = " ";
+      dateRegister.innerHTML = " ";
     }, 4000);
   } else {
-    datosRegis.innerHTML = "Enviado";
-    datosRegis.classList.add("correct");
-    datosRegis.classList.remove("danger");
+    dateRegister.innerHTML = "Enviado";
+    dateRegister.classList.add("correct");
+    dateRegister.classList.remove("danger");
     setTimeout(() => {
-      datosRegis.innerHTML = " ";
-      nombre.value = "";
+      dateRegister.innerHTML = " ";
+      name.value = "";
       email.value = "";
     }, 2000);
   }
+
 }
+
+
+
+
