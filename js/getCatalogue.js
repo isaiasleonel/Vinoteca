6a9496e9e8635afd6820743eafe9f9ea0a8642e6
Isaias Leonel Sardina <isaias.sardina@gmail.com>
2023-08-19 19:33:46 -0300

    Se agrega Search Filter en header anexado con Catalogo.html y se mejora estetica del Form de Contacto.html

--- js/getCatalogue.js
+++ js/getCatalogue.js
@@ -14,7 +14,7 @@ function mostrar() {
                 <div class="card-container__date">
                     <p class="oferta"> ${vino.region}</p>
                 </div>
-                <p class="card-container__price">$${vino.price}</p>
+                <p class="card-container__price">$USD ${vino.price}</p>
                 <button id="btn-catalogue-${vino.id} " class="card-container__btn-pay">AGREGAR</button>
             </div>
         </div>
@@ -23,3 +23,30 @@ function mostrar() {
 }
 mostrar();
 
+const searchInput = document.querySelector("#search");
+const cards = document.querySelectorAll(".card-container");
+
+function filterCards(searchQuery) {
+    let nothingFound = document.querySelector("#nothing-alert");
+    let number = 0;
+    cards.forEach(function (card) {
+        let name = card.querySelector(".card-container__trip-name").textContent.toLowerCase();
+
+        if (name.includes(searchQuery.toLowerCase())) {
+            nothingFound.style.display = "none";
+            card.style.display = "block";
+            number++;
+        }
+        else {
+            card.style.display = "none";
+        }
+    });
+    if (number == 0) {
+        nothingFound.style.display = "block";
+    }
+}
+
+searchInput.addEventListener("input", function () {
+    const searchQuery = searchInput.value.trim();
+    filterCards(searchQuery);
+});
