c4fd86f01636f3e963935a21efe4d92b5a45add6
Isaias Leonel Sardina <isaias.sardina@gmail.com>
2023-08-21 20:18:27 -0300

    Se realiza el  Responsive

--- js/modalCatalogue.js
+++ js/modalCatalogue.js
@@ -12,3 +12,9 @@ close.addEventListener("click", (e) => {
     e.preventDefault();
     modal.classList.remove("modal--show");
 });
+
+// ------------------btn-menu-toggle-------------
+const menu = document.querySelector(".header_contenedor1");
+const btnMenu = document.querySelector("#btn-menu").addEventListener("click", (e) => {
+    menu.classList.toggle("activado");
+});
